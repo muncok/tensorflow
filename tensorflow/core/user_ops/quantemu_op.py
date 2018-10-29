555f4623ece26a46592fd8bf4c4073f33b0b314d
Naveen Mellempudi <navikm@gmail.com>
2018-10-29 11:50:05 -0700

    minor changes

--- tensorflow/core/user_ops/quantemu_op.py
+++ tensorflow/core/user_ops/quantemu_op.py
@@ -10,7 +10,7 @@ import os
 import tensorflow as tf
 
 @tf_export("quantize_emu")
-def quantize_emu(input, data_format='channels_last', allocate_copy=0, output_data_type=0, output_precision=23, output_exponent_bits=5, channel_blocking_type=0, input_channels_per_block=4096, round_mode=0, quantize_gradients=0, quantize_gradients_only=0):
+def quantize_emu(input, data_format='channels_last', allocate_copy=0, output_data_type=0, output_unsigned=0, output_precision=23, output_exponent_bits=5, channel_blocking_type=0, input_channels_per_block=4096, round_mode=0, quantize_gradients=0, quantize_gradients_only=0):
   #_quantemu_module = tf.load_op_library(os.path.join(tf.resource_loader.get_data_files_path(), 'quantemu.so'))
   _quantemu_module = tf.load_op_library(os.path.join(tf.resource_loader.get_root_dir_with_all_resources(), '../../core/user_ops/quantemu.so'))
   if quantize_gradients_only is 0:
@@ -19,6 +19,7 @@ def quantize_emu(input, data_format='channels_last', allocate_copy=0, output_dat
 		data_format, 
 		allocate_copy, 
 		output_data_type, 
+		output_unsigned, 
 		output_precision, 
 		output_exponent_bits, 
 		channel_blocking_type, 
@@ -38,6 +39,7 @@ def _quantize_emu_grad(op, grad):
 		data_format=op.get_attr("data_format"), 
 		allocate_copy=int(os.getenv('QUANTEMU_ALLOCATE_COPY_GRAD', 0)), 
 		output_data_type=op.get_attr("output_data_type"), 
+		output_unsigned=op.get_attr("output_unsigned"), 
 		output_precision=int(os.getenv('QUANTEMU_PRECISION_GRAD', 23)), 
 		output_exponent_bits=op.get_attr("output_exponent_bits"), 
 		channel_blocking_type=int(os.getenv('QUANTEMU_CBLOCK_TYPE_GRAD', 0)), 
